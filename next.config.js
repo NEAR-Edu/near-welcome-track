df2b084b6b3f725100ccae745451b612e9987df0
Jacob <encody@users.noreply.github.com>
2022-03-29 14:24:59 -0500

    adds svg inlining

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack(config) {
+    // Enable SVG inlining
+    // https://dev.to/dolearning/importing-svgs-to-next-js-nna#svgr
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+
+    return config;
+  },
 };
 
 module.exports = nextConfig;
