740bb25198eba23fa3bc3d9e7a67082ab8fc0162
Jacob <encody@users.noreply.github.com>
2022-03-29 15:00:10 -0500

    header + footer prototype

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,9 @@
-import { AppShell, Footer, Header } from '@mantine/core';
+import { Anchor, AppShell, Footer, Header } from '@mantine/core';
 import type { NextPage } from 'next';
 import Head from 'next/head';
 
 import Logo from '../svg/logo.svg';
+import Near from '../svg/near.svg';
 
 const Home: NextPage = () => {
   return (
@@ -16,14 +17,21 @@ const Home: NextPage = () => {
       <AppShell
         padding="md"
         header={
-          <Header height={60} p="xs">
-            Header <Logo />
+          <Header height={60} p="md" className="flex items-center border-0">
+            <Logo className="h-full" />
           </Header>
         }
-        footer={<Footer height={60}>t</Footer>}
-        styles={(theme) => ({
-          main: { backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[8] : theme.colors.gray[0] },
-        })}
+        footer={
+          <Footer height={60} p="md" className="flex items-center justify-between border-0">
+            <Anchor href="https://near.university" className="text-gray-400">
+              NEAR Education, 2022
+            </Anchor>
+            <Anchor href="https://near.org" className="text-gray-400 inline-flex items-center">
+              <span className="mr-3">Powered by</span>
+              <Near className="h-6" />
+            </Anchor>
+          </Footer>
+        }
       >
         My application
       </AppShell>
