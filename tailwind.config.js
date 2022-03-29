5f1154e34791a5e3bf22fc16b87bb935c1df0032
Jacob <encody@users.noreply.github.com>
2022-03-29 14:59:46 -0500

    fonts + svg

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@ module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
+    fontFamily: {
+      sans: ['Manrope', 'Arial', 'ui-sans-serif', 'system-ui', 'sans-serif'],
+    },
   },
   plugins: [],
 };
