bcc7b6fb3dc3b151c72ced4bfcfc58b9d5d03f26
Petar Vujović <petarvujovic98@gmail.com>
2022-03-29 13:40:52 +0200

    Add Prisma and set up basic configuration

---
+++ src/lib/db.ts
@@ -0,0 +1,13 @@
+import { PrismaClient } from '@prisma/client';
+
+declare global {
+  // allow global `var` declarations https://www.prisma.io/docs/support/help-articles/nextjs-prisma-client-dev-practices#solution
+  // eslint-disable-next-line no-var,vars-on-top
+  var prisma: PrismaClient | undefined;
+}
+
+const prisma = global.prisma || new PrismaClient();
+
+export default prisma;
+
+if (process.env.NODE_ENV !== 'production') global.prisma = prisma;
