7c1fb4d60ac8c07d262e36a7d36499be508d325b
Petar Vujović <petarvujovic98@gmail.com>
2022-03-30 14:47:58 +0200

    Change main header style

--- src/components/MainHeader.tsx
+++ src/components/MainHeader.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
 
-const MainHeader: React.FC = ({ children }) => <h1 className="font-bold text-6xl text-gray-900">{children}</h1>;
+const MainHeader: React.FC = ({ children }) => <h1 className="font-bold text-heading-1 text-primary-gray-6">{children}</h1>;
 
 export default MainHeader;
