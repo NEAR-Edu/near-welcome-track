3a3228e240ac9aac6d3ba67a662112bdd74a8858
Petar Vujović <petarvujovic98@gmail.com>
2022-04-01 17:22:19 +0200

    Open links in new tabs

--- src/components/CardLink.tsx
+++ src/components/CardLink.tsx
@@ -7,7 +7,12 @@ interface Props {
 }
 
 const CardLink: React.FC<Props> = ({ href, text, completed }) => (
-  <a href={href} className={`flex-1 text-primary-black hover:text-primary-orange visited:text-primary-gray-4 visited:line-through ${completed ? 'line-through' : ''}`}>
+  <a
+    href={href}
+    target="_blank"
+    className={`flex-1 text-primary-black hover:text-primary-orange visited:text-primary-gray-4 visited:line-through ${completed ? 'line-through' : ''}`}
+    rel="noreferrer"
+  >
     {text}
     <span className="decoration-transparent">{LINK_END_ARROW}</span>
   </a>
